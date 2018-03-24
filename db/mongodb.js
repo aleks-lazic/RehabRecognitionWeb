417b35786814b4b8144ced5d1023b1ec1e5f2b33
aleks-lazic <aleksandar.lazic@students.hevs.ch>
2018-03-24 16:35:15 +0100

    find all patients from db mongo

---
+++ db/mongodb.js
@@ -0,0 +1,34 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+/**
+ * connection to the database
+ */
+var connect = function(){
+    return new Promise((resolve, reject) => {
+        MongoClient.connect(url, (err, client) => {
+            if(err)
+                throw err;
+            console.log("Connection successfull");
+            resolve(client);
+        })
+    })
+};
+
+/**
+ * find all patients
+ * @param {string} dbName 
+ */
+var findAllPatients = function(dbName){
+    connect().then((client) => {
+        var db = client.db(dbName);
+        db.collection('patient').find({}).toArray((err, result) => {
+            console.log(result);
+            client.close();
+        })
+    })
+}
+
+
+
+ exports.findAllPatients = findAllPatients;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('../db/mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RehabCoach' });
+  mongo.findAllPatients('rehab_coach');
 });
 
 module.exports = router;
